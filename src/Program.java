b628c0b904fa1e98f59c6e1abbf0106b8d033096
ivivanov <me@ivivanov.com>
2014-09-20 14:28:16 +0300

    test cases added

--- src/Program.java
+++ src/Program.java
@@ -3,27 +3,22 @@ import java.net.URISyntaxException;
 
 public class Program {
 	public static void main(String[] args) {
-		// String root = "http://fmi.wikidot.com";
-		// Crowler cr = new Crowler();
-		// String result = cr.crowl(root, "Докажете, че:");
-		// System.out.println(result);
 		try {
 			WebCrawler crawler = new WebCrawler();
 			URI startLocation;
 
-			startLocation = new URI("http://ebusiness.free.bg");
-			URI link = crawler.crawl(startLocation, "Револвираща");
-			System.out.println("result :" + link.toString());
+			// startLocation = new URI("http://ebusiness.free.bg");
+			// URI link = crawler.crawl(startLocation, "Револвираща");
+			// System.out.println("result :" + link.toString());
 			// http://ebusiness.free.bg/cards_bank_cards.html
 
-			// startLocation = new URI("http://blog.hackbulgaria.com");
-			// link = crawler
-			// .crawl(startLocation,
-			// "Като страничен ефект, особено при момчетата, може да бъде бързо-растяща брада.");
-			//
-			// System.out.println("result :" + link.toString());
+			startLocation = new URI("http://blog.hackbulgaria.com");
+			URI link = crawler.crawl(startLocation,
+							"Като страничен ефект, особено при момчетата, може да бъде бързо-растяща брада.");
+
+			System.out.println("result :" + link.toString());
 			// http://blog.hackbulgaria.com/fall-of-the-hackers/
-			//
+			
 			// startLocation = new URI("http://fmi.wikidot.com");
 			// link = crawler.crawl(startLocation, "Докажете, че:");
 			//
