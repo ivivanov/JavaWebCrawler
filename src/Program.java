045e8b7db9a9741b4c55ee92ea78d712a6e86b38
ivivanov <me@ivivanov.com>
2014-09-21 12:52:31 +0300

    fixed url creation, clear map before every crawl

--- src/Program.java
+++ src/Program.java
@@ -1,44 +1,44 @@
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.List;
 
 public class Program {
 	public static void main(String[] args) {
+
 		try {
 			WebCrawler crawler = new WebCrawler();
 			URI startLocation;
 
-//			startLocation = new URI("http://ebusiness.free.bg");
-//			URI link = crawler.crawl(startLocation, "Револвираща");
-//			System.out.println("result :" + link.toString());
-			// http: // ebusiness.free.bg/cards_bank_cards.html
-			startLocation = new URI("http://zeron.bg");
-			URI link = crawler.crawl(startLocation, "V/4 поддържа работа освен на български език и още на 12 езика, съответно:");
+			startLocation = new URI("http://ebusiness.free.bg");
+			URI link = crawler.crawl(startLocation, "Револвираща");
 			System.out.println("result :" + link.toString());
+			crawler.printPath(link.toString());
+			// http: // ebusiness.free.bg/cards_bank_cards.html
 
-			//	http://zeron.bg/Products/Multilanguage
-			// startLocation = new URI("http://ivivanov.com");
-			// URI link = crawler.crawl(startLocation,
-			// " sequence of Fibonacci");
-			// System.out.println("result :" + link.toString());
+			startLocation = new URI("http://zeron.bg");
+			link = crawler.crawl(startLocation,
+					"поддържа работа освен на български език и още на 12");
+			System.out.println("result :" + link.toString());
+			crawler.printPath(link.toString());
+			// http://zeron.bg/Products/Multilanguage
 
-			// startLocation = new URI("http://blog.hackbulgaria.com");
-			// URI link = crawler
-			// .crawl(startLocation,
-			// "Като страничен ефект, особено при момчетата, може да бъде бързо-растяща брада.");
-			// System.out.println("result :" + link.toString());
+			startLocation = new URI("http://blog.hackbulgaria.com");
+			link = crawler
+					.crawl(startLocation,
+							"Като страничен ефект, особено при момчетата, може да бъде бързо-растяща брада.");
+			System.out.println("result :" + link.toString());
+			crawler.printPath(link.toString());
 			// http://blog.hackbulgaria.com/fall-of-the-hackers/
 
-//			startLocation = new URI("http://fmi.wikidot.com");
-//			URI link = crawler.crawl(startLocation, "Докажете, че:");
-//			System.out.println("result :" + link.toString());
+			 startLocation = new URI("http://fmi.wikidot.com");
+			 link = crawler.crawl(startLocation, "Докажете, че:");
+			 System.out.println("result :" + link.toString());
+			 crawler.printPath(link.toString());
 			// http: // fmi.wikidot.com/anal-examination-1
 
-			//find all
-//			List<URI> allResults = crawler.findAll(new URI("http://comac-medical.com"), "Vrabevski");
-//			System.out.print(allResults.toString());
-			
-			System.out.print(crawler.backtrack(link.toString()));
+			// find all
+			// List<URI> allResults = crawler.findAll(new
+			// URI("http://comac-medical.com"), "Vrabevski");
+			// System.out.print(allResults.toString());
 		} catch (URISyntaxException e) {
 			System.out.println("Invalid url");
 		}
--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -14,6 +14,7 @@ import java.util.regex.Pattern;
 public class WebCrawler {
 
 	private final String ExtractUrlFromAnchorPattern = "(?<=<a href=\"{1}).+?(?=\")";
+	private final String AnchorBeginningPattern = "^.?.?/";
 	// private final String UrlPattern =
 	// "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
 	private Map<String, String> urlParentUrl;
@@ -22,64 +23,8 @@ public class WebCrawler {
 		this.urlParentUrl = new HashMap<String, String>();
 	}
 
-	private LinkedList<String> getAllLinks(String html) {
-		Pattern regex = Pattern.compile(ExtractUrlFromAnchorPattern);
-		Matcher m = regex.matcher(html);
-		LinkedList<String> urls = new LinkedList<String>();
-		while (m.find()) {
-			urls.add(html.substring(m.start(), m.end()));
-		}
-
-		return urls;
-	}
-
-	private String getHtml(URI uri) {
-		StringBuilder sb = new StringBuilder();
-		try {
-			URL url = new URL(uri.toString());
-			URLConnection conn = url.openConnection();
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					conn.getInputStream(), "UTF8"));
-			String inputLine;
-
-			while ((inputLine = br.readLine()) != null) {
-				sb.append(inputLine);
-			}
-
-			br.close();
-		} catch (MalformedURLException e) {
-			// e.printStackTrace();
-		} catch (IOException e) {
-			// e.printStackTrace();
-		}
-
-		return sb.toString();
-	}
-
-	private boolean isFullUrl(String input) {
-		// Pattern regex = Pattern.compile(ExtractUrlFromAnchorPattern);
-		// Matcher m = regex.matcher(input);
-		// return m.find();
-		try {
-			@SuppressWarnings("unused")
-			URL temp = new URL(input);
-		} catch (MalformedURLException e) {
-			return false;
-		}
-		return true;
-	}
-
-	private URI createURI(String link) {
-		URI result;
-		try {
-			result = new URI(link);
-			return result;
-		} catch (URISyntaxException e) {
-			return null;
-		}
-	}
-
 	public URI crawl(URI root, String needle) {
+		this.urlParentUrl.clear();
 		Queue<URI> uriQueue = new LinkedList<URI>();
 		uriQueue.add(root);
 		urlParentUrl.put(root.toString(), "");
@@ -98,21 +43,12 @@ public class WebCrawler {
 
 			for (int i = 0; i < linksCount; i++) {
 				String tempUrl = links.get(i);
-				String fullUrl = "";
+				String corectUrl = buildCorectUrl(tempUrl, root.toString());
 
-				if (isFullUrl(tempUrl)) {
-					if (tempUrl.contains(root.toString())) {
-						fullUrl = tempUrl;
-					} else {
-						fullUrl = "";
-					}
-				} else {
-					fullUrl = root + "/" + tempUrl;
-				}
-
-				if (fullUrl.length() > 0 && !urlParentUrl.containsKey(fullUrl)) {
-					urlParentUrl.put(fullUrl, currentUri.toString());
-					uriQueue.add(createURI(fullUrl));
+				if (corectUrl.length() > 0
+						&& !urlParentUrl.containsKey(corectUrl)) {
+					urlParentUrl.put(corectUrl, currentUri.toString());
+					uriQueue.add(createURI(corectUrl));
 				}
 			}
 		}
@@ -161,11 +97,15 @@ public class WebCrawler {
 		return result;
 	}
 
-	public List<String> backtrack(String url) {
+	public void printPath(String url){
+		System.out.println(this.getPath(url));
+	}
+	
+	public List<String> getPath(String url) {
 		List<String> path = new LinkedList<String>();
 		path.add(url);
 		String parent = this.urlParentUrl.get(url);
-		while (parent.length() > 0) {
+		while (parent != null && parent.length() > 0) {
 			path.add(parent);
 			String tmp = this.urlParentUrl.get(parent);
 			parent = tmp;
@@ -178,5 +118,78 @@ public class WebCrawler {
 		}
 
 		return reversedPath;
+	}
+	
+	private LinkedList<String> getAllLinks(String html) {
+		Pattern regex = Pattern.compile(ExtractUrlFromAnchorPattern);
+		Matcher m = regex.matcher(html);
+		LinkedList<String> urls = new LinkedList<String>();
+		while (m.find()) {
+			urls.add(html.substring(m.start(), m.end()));
+		}
+
+		return urls;
+	}
+
+	private String getHtml(URI uri) {
+		StringBuilder sb = new StringBuilder();
+		try {
+			URL url = new URL(uri.toString());
+			URLConnection conn = url.openConnection();
+			InputStreamReader reader = new InputStreamReader(
+					conn.getInputStream());
+			BufferedReader br = new BufferedReader(reader);
+			String inputLine;
+
+			while ((inputLine = br.readLine()) != null) {
+				sb.append(inputLine);
+			}
+
+			br.close();
+		} catch (MalformedURLException e) {
+			// e.printStackTrace();
+		} catch (IOException e) {
+			// e.printStackTrace();
+		}
+
+		return sb.toString();
+	}
+
+	private boolean isFullUrl(String input) {
+		// Pattern regex = Pattern.compile(ExtractUrlFromAnchorPattern);
+		// Matcher m = regex.matcher(input);
+		// return m.find();
+		try {
+			@SuppressWarnings("unused")
+			URL temp = new URL(input);
+		} catch (MalformedURLException e) {
+			return false;
+		}
+		return true;
+	}
+
+	private URI createURI(String link) {
+		URI result;
+		try {
+			result = new URI(link);
+			return result;
+		} catch (URISyntaxException e) {
+			return null;
+		}
+	}
+
+	private String buildCorectUrl(String tempUrl, String root) {
+		String corectUrl = "";
+		if (isFullUrl(tempUrl)) {
+			if (tempUrl.contains(root)) {
+				corectUrl = tempUrl;
+			} else {
+				corectUrl = "";
+			}
+		} else {
+			corectUrl = root + "/"
+					+ tempUrl.replaceAll(AnchorBeginningPattern, "");
+		}
+		return corectUrl;
 	}
 }
